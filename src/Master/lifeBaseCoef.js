d2f301581b6bb082f7c40673a322a132995b3fe2
Shuhei Nomura <info@danmaq.com>
2017-12-13 21:52:23 +0900

    Add master.

---
+++ src/Master/lifeBaseCoef.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** Not a Number. */
+const N = Number.NaN;
+
+const table = [
+    [{ t: 9, v: 10 }, { t: 13, v: 8 }, { t: N, v: 6 }],
+    [{ t: 3, v: 6 }, { t: 5, v: 5 }, { t: 14, v: 3 }, { t: N, v: 1 }],
+    [{ t: 10, v: 1 }, { t: N, v: 2 }],
+    [{ t: 9, v: 2 }, { t: 13, v: 10 }, { t: N, v: 5 }],
+    [{ t: 5, v: 5 }, { t: 14, v: 7 }, { t: N, v: 3 }],
+    [{ t: 10, v: 3 }, { t: N, v: 4 }],
+    [{ t: 9, v: 4 }, { t: 13, v: 2 }, { t: N, v: 6 }],
+    [{ t: 3, v: 6 }, { t: 5, v: 5 }, { t: 14, v: 9 }, { t: N, v: 7 }],
+    [{ t: 10, v: 7 }, { t: N, v: 8 }],
+    [{ t: 9, v: 8 }, { t: 13, v: 4 }, { t: N, v: 5 }],
+    [{ t: 5, v: 5 }, { t: 14, v: 1 }, { t: N, v: 9 }],
+    [{ t: 10, v: 9 }, { t: N, v: 10 }],
+];
+
+module.exports =
+    (month = 1, b9 = 0, b10 = 0) =>
+    (row =>
+        !row ? N :
+        row.find(v => Number.isNaN(v.t) || b9 < b10 + v.t).v
+    )(table[month - 1]);
