ef63fc705b09e5bab81ba637a3d84cfd866c5a0e
Shuhei Nomura <info@danmaq.com>
2017-12-15 15:22:55 +0900

    Refactored.

--- src/Master/naturesDesc.js
+++ src/Master/naturesDesc.js
@@ -30,19 +30,20 @@ const generate = (...row) =>
         motivation: motivation[row[4]],
     })
 
+const map =
+    new Map([
+        ['A000', generate(1, 1, 1, 1, 5)],
+        ['A024', generate(1, 0, 0, 0, 2)],
+        ['A100', generate(1, 0, 1, 2, 5)],
+        ['A888', generate(1, 1, 0, 3, 2)],
+        ['E001', generate(0, 1, 1, 1, 1)],
+        ['E125', generate(0, 0, 0, 0, 0)],
+        ['E555', generate(0, 0, 1, 2, 1)],
+        ['E919', generate(0, 1, 0, 3, 0)],
+        ['H012', generate(0, 1, 0, 3, 4)],
+        ['H025', generate(0, 0, 0, 0, 4)],
+        ['H108', generate(1, 1, 1, 1, 3)],
+        ['H789', generate(1, 0, 1, 2, 3)],
+    ]);
 /** Natures description. */
-module.exports =
-    Object.freeze({
-        A000: generate(1, 1, 1, 1, 5),
-        A024: generate(1, 0, 0, 0, 2),
-        A100: generate(1, 0, 1, 2, 5),
-        A888: generate(1, 1, 0, 3, 2),
-        E001: generate(0, 1, 1, 1, 1),
-        E125: generate(0, 0, 0, 0, 0),
-        E555: generate(0, 0, 1, 2, 1),
-        E919: generate(0, 1, 0, 3, 0),
-        H012: generate(0, 1, 0, 3, 4),
-        H025: generate(0, 0, 0, 0, 4),
-        H108: generate(1, 1, 1, 1, 3),
-        H789: generate(1, 0, 1, 2, 3),
-    });
+module.exports = (key = '') => map.get(key);
--- src/index.js
+++ src/index.js
@@ -25,7 +25,14 @@ const ymd =
             date: ld.dayOfMonth(),
             dcoef
         };
-    }
+    };
+
+const naturePotential =
+    (cycle = 0) =>
+    (code => ({ mn: code(natures, 12), mp: code(potential, 10) }))(
+        (func = ({ x = 0, y = 0 }) => `${''}`, limit = 0) =>
+        (v = 0) =>
+        func({ x: (v % limit || limit) - 1, y: cycle }));
 
 module.exports =
     (birth = new Date()) => {
@@ -42,27 +49,22 @@ module.exports =
                 date + 1
             )((f = 0, v = 0) => (f * v) >> 0)
         const dGEcof = date >= dcoef;
-        const zero = (l = 0, v = 0) => v || l;
-        const outer = zero(12, month - !dGEcof >> 0) + 1;
+        const outer = (month - (!dGEcof >> 0) || 12) + 1;
         const cycle = (inner + 6) % 10;
-        const mz = (l = 0, v = 0) => ({ x: zero(l, v % l) - 1, y: cycle });
-        const mn = (v = 0) => natures(mz(12, v));
-        const mp = (v = 0) => potential(mz(10, v));
-        const mnd =
+        const { mn, mp } = naturePotential(cycle);
+        const desc =
             (v = 0) => {
                 const nature = mn(v);
-                let d = naturesDesc.A000;
-                d = naturesDesc[nature];
-                return { ...d, nature };
+                return { ...naturesDesc(nature), nature };
             };
-        const ymb3 = yh * 100 + yl - (month === 2 && dGEcof ? m12 : 0);
-        const lbx = lifeBaseCoef(month, date, dcoef) - 1;
+        const ymb = yh * 100 + yl - (month === 2 && dGEcof ? m12 : 0);
+        const lbp = { x: lifeBaseCoef(month, date, dcoef) - 1, y: cycle };
         return {
-            inner: mnd(inner + yh * 4 + mb3 * 6),
-            outer: mnd(outer),
-            cycle: zero(10, cycle),
-            lifeBase: lifeBase({ x: lbx, y: cycle }),
-            potential: `${mp(ymb3 + 7)}-${mp(year * 2 + outer + 2)}`,
-            workstyle: mn(ymb3 + 9),
+            inner: desc(inner + yh * 4 + mb3 * 6),
+            outer: desc(outer),
+            cycle: cycle || 10,
+            lifeBase: lifeBase(lbp),
+            potential: `${mp(ymb + 7)}-${mp(year * 2 + outer + 2)}`,
+            workstyle: mn(ymb + 9),
         };
     };
